2edfc4266329d5eed6a9ad5db93df8e7a58292ac
dineshrajdhanapathyDD <dineshrajdhanapathy@gmail.com>
2025-12-24 00:18:54 +0530

    update build

--- api/nearby-attractions.d.ts
+++
@@ -1,3 +0,0 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-export default function handler(req: VercelRequest, res: VercelResponse): Promise<void>;
-//# sourceMappingURL=nearby-attractions.d.ts.map
